300850e005fbec275ec935d93ee27657e8697574
Dominic Watts <dominic@pixie.agency>
2023-11-23 23:10:08 +0000

    template suggestions

--- Block/Suggested.php
+++ Block/Suggested.php
@@ -2,12 +2,12 @@
 
 namespace PixieMedia\Suggestion\Block;
 
+use Magento\Catalog\Api\CategoryRepositoryInterface;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
 use Magento\Framework\Registry;
-use Magento\Framework\View\Element\Template;
 use PixieMedia\Suggestion\Model\ResourceModel\Related\CollectionFactory as RelatedCollectionFactory;
 
-class Suggested extends Template
+class Suggested extends \Magento\Catalog\Block\Product\ListProduct
 {
     /**
      * @var \Magento\Framework\Registry
@@ -35,15 +35,24 @@ class Suggested extends Template
     private $productCollectionFactory;
 
     /**
-     * @param Template\Context $context
+     * @param \Magento\Catalog\Block\Product\Context $context
+     * @param \Magento\Framework\Data\Helper\PostHelper $postDataHelper
+     * @param \Magento\Catalog\Model\Layer\Resolver $layerResolver
+     * @param CategoryRepositoryInterface $categoryRepository
+     * @param \Magento\Framework\Url\Helper\Data $urlHelper
      * @param \Magento\Framework\Registry $registry
      * @param \PixieMedia\Suggestion\Model\ResourceModel\Related\CollectionFactory $relatedCollectionFactory
      * @param \Magento\Catalog\Model\Product\Visibility $productVisibility
      * @param \Magento\CatalogInventory\Helper\Stock $stockHelper
+     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
      * @param array $data
      */
     public function __construct(
-        Template\Context $context,
+        \Magento\Catalog\Block\Product\Context $context,
+        \Magento\Framework\Data\Helper\PostHelper $postDataHelper,
+        \Magento\Catalog\Model\Layer\Resolver $layerResolver,
+        CategoryRepositoryInterface $categoryRepository,
+        \Magento\Framework\Url\Helper\Data $urlHelper,
         Registry $registry,
         RelatedCollectionFactory $relatedCollectionFactory,
         \Magento\Catalog\Model\Product\Visibility $productVisibility,
@@ -56,7 +65,14 @@ class Suggested extends Template
         $this->productVisibility = $productVisibility;
         $this->stockHelper = $stockHelper;
         $this->productCollectionFactory = $productCollectionFactory;
-        parent::__construct($context, $data);
+        parent::__construct(
+            $context,
+            $postDataHelper,
+            $layerResolver,
+            $categoryRepository,
+            $urlHelper,
+            $data
+        );
     }
 
     public function getSuggested()
@@ -88,6 +104,14 @@ class Suggested extends Template
                 $array[$id] = $id;
             }
         }
+        if (empty($array)) {
+            return [];
+        }
+
+        // remove current product
+        $currentProduct = $this->registry->registry('current_product');
+        unset($array[$currentProduct->getId()]);
+
         return $this->getVisibleProductsById($array);
     }
 
@@ -99,6 +123,7 @@ class Suggested extends Template
     {
         $collection = $this->productCollectionFactory
             ->create()
+            ->addAttributeToSelect('*')
             ->addIdFilter($ids)
             ->addAttributeToFilter('status', ['eq' => 1])
             ->setVisibility($this->productVisibility->getVisibleInSiteIds());
